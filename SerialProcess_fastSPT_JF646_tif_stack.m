ce3c22ee1a9f53d73bd638b449dcf498e49e598e
lukasfue <lukas.fuentes@yale.edu>
2018-03-09 16:31:24 -0500

    Upload of script that loops over tiff stacks

--- SerialProcess_fastSPT_JF646_tif_stack.m
+++ SerialProcess_fastSPT_JF646_tif_stack.m
@@ -1,8 +1,10 @@
+%   Commit Test
 %   SerialProcess_fastSPT_JF646.m
 %   Anders Sejr Hansen, August 2016
 %%% Edit of Ander Sejr Hansen's code to no longer take nd2 files, but
 %%% instead read in Tiff stacks into a 3D matrix and input that into the
 %%% MTT algorithm
+
 clear; clc; close all; clearvars -global
 
 %   DESCRIPTION
@@ -14,9 +16,9 @@ clear; clc; close all; clearvars -global
 
 %%%%%%%%%%%%%%%%%%%% DEFINE INPUT AND OUTPUT PATHS %%%%%%%%%%%%%%%%%%%%%%%%
 % Specify input path with Tiff stack files: The path written as one of the
-input_path = uigetdir('C:\Users\Bewersdorf\Desktop\Lukas Fuentes\Data', 'Select Input Data File');
+input_path = uigetdir('C:\Users\Bewersdorf\Desktop\Lukas Fuentes\Data', 'Select Input Data Folder');
 % Specify output path for results
-output_path= uigetdir('C:\Users\Bewersdorf\Desktop\Lukas Fuentes\Data', 'Select Output File');
+output_path= uigetdir('C:\Users\Bewersdorf\Desktop\Lukas Fuentes\Data', 'Select Output Folder');
 % uigetdir functions parameters needs to be changed to be specific to your
 % system for both input and output paths. This path is just the starting
 % path for the uigetdir function.
@@ -35,9 +37,9 @@ end
 %%% Parameters of particle tracking %%%
 LocalizationError = -6.25; % Localization Error: -6 = 10^-6
 EmissionWavelength = 664; % wavelength in nm; consider emission max and filter cutoff
-ExposureTime = 6; % in milliseconds
+ExposureTime = 20; % in milliseconds
 NumDeflationLoops = 0; % Generaly keep this to 0; if you need deflation loops, you are imaging at too high a density;
-MaxExpectedD = 20; % The maximal expected diffusion constant for tracking in units of um^2/s;
+MaxExpectedD = 10; % The maximal expected diffusion constant for tracking in units of um^2/s;
 NumGapsAllowed = 1; % the number of gaps allowed in trajectories
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -88,20 +90,6 @@ addpath('C:\Users\Bewersdorf\Desktop\Lukas Fuentes\Single particle tracking\2017
 addpath('C:\Users\Bewersdorf\Desktop\Lukas Fuentes\Single particle tracking\20170727_Maxime\SLIMFAST_batch_fordist\bfmatlab');
 disp('added paths for MTT algorithm mechanics, bioformats...');
 
-%%% For uploading Tiff stack %%%
-FileTif = uigetfile; %was '1_sp_stack.tif'
-InfoImage = imfinfo(FileTif);
-mImage = InfoImage(1).Width;
-nImage = InfoImage(1).Height;
-NumberImages = length(InfoImage);
-tif_files = zeros(nImage,mImage,NumberImages,'uint16');
-TifLink = Tiff(FileTif, 'r');
-for i = 1:NumberImages
-   TifLink.setDirectory(i);
-   tif_files(:,:,i) = TifLink.read();
-end
-TifLink.close();
-
 files = dir(input_path);
 Filenames = ''; %for saving the actual file name
 
@@ -114,6 +102,20 @@ for iter = 3:length(Filenames)
     tic; 
     disp(['reading in Tiff stack file ', num2str(iter - 2), ' of ', num2str(length(Filenames)-2), ' total Tiff stack files']);
 
+    %%% For uploading Tiff stack %%%
+    FileTif = files(iter).name; %uigetfile
+    InfoImage = imfinfo(FileTif);
+    mImage = InfoImage(1).Width;
+    nImage = InfoImage(1).Height;
+    NumberImages = length(InfoImage);
+    tif_files = zeros(nImage,mImage,NumberImages,'uint16');
+    TifLink = Tiff(FileTif, 'r');
+    for i = 1:NumberImages
+        TifLink.setDirectory(i);
+        tif_files(:,:,i) = TifLink.read();
+    end
+    TifLink.close();
+    
     imgs_3d_matrix = tif_files;
     %convert to double, re-scale to 16 bit:
     imgs_3d_double = (2^16-1)*im2double(imgs_3d_matrix);
@@ -161,9 +163,14 @@ for iter = 3:length(Filenames)
         trackedPar(i).TimeStamp = impars.FrameRate.* data_cell_array{i}(:,3);
     end
     disp(['Localized and tracked ', num2str(length(trackedPar)), ' trajectories']);
+    % Next four lines are for fixing output file problem (outputs to wrong file) 3/2/18 - LAF
+    % mkdir output_path Results_{iter}
+    % eval(['mkdir ' output_path ' Results_' num2str(iter);])
+    % save_name = fullfile(output_path, Filenames{iter}, '_Tracked.mat');
+    % save(save_name, 'trackedPar', 'settings');
     save([output_path, Filenames{iter}, '_Tracked.mat'], 'trackedPar', 'settings');
     toc;
-    clear imgs_3d_matrix imgs_3d_double data_cell_array trackedPar
+    clear imgs_3d_matrix imgs_3d_double data_cell_array trackedPar save_name
     disp('-----------------------------------------------------------------');
 end    
 
